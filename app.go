5207904ff2301e7fd00e4f481f14715a38a57033
mindon <mindon@gmail.com>
2016-02-23 10:38:42 +0800

    Create app.go

---
+++ app.go
@@ -0,0 +1,19 @@
+package weplux
+
+import (
+    "fmt"
+    "time"
+    "net/http"
+)
+
+func init() {
+  http.HandleFunc("/hello", homeHandler)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	var now = time.Now();
+  fmt.Fprintf(w, "Hello, Wellcome to WePlux - by Mindon!<hr>%v, %v/365 = %.2f%% ",
+    now.Format("2006年01月02日 时区 Z07:00 15:04:05.000  Mon"),
+    now.YearDay(),
+    float64( 100 * now.YearDay()) / 365.0)
+}
