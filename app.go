02a5638ec7ca13d0bc1cf65f7a10e584cc24db3d
mindon <mindon@qq.com>
2016-02-23 12:01:19 +0800

    Update app.go from local

--- app.go
+++ app.go
@@ -12,8 +12,10 @@ func init() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
   var now = time.Now();
-  fmt.Fprintf(w, "Hello, Wellcome to WePlux - by Mindon!<hr>%v, %v/365 = %.2f%% ",
+  fmt.Fprintf(w, "Hello, Wellcome to WePlux - by Mindon!\n%v, %v/365 = %.2f%% ",
     now.Format("2006年01月02日 时区 Z07:00 15:04:05.000  Mon"),
     now.YearDay(),
     float64( 100 * now.YearDay()) / 365.0)
+
+  fmt.Fprint(w, "\n\nhttp://www.pinqic.com/")
 }
